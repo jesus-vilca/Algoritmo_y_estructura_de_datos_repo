4fdc3dcf472c31eeb4b449e8147afb2d8edf7aac
jesus-vilca <73800484+jesus-vilca@users.noreply.github.com>
2021-11-05 08:51:25 -0500

    Update dispersalista.h

--- lab 08/dispersalista.h
+++ lab 08/dispersalista.h
@@ -8,7 +8,7 @@ class DispersaLista
     private:
         Nodo<T>* punta;
     public:
-        DispersaLista( T mat** , int n , int m );
+        DispersaLista(int n , int m);
         void mostrarMayorFila();
         int sumaColumnas();
         void mostrarParesFilas();
@@ -56,8 +56,65 @@ class DispersaLista
         void eliminarPares();
         void eliminarMayor();
         void eliminarMayorFila();
+        Nodo<T> buscar(int f, int c);
 
 };
-
+template<typename T>
+DispersaLista<T>::DispersaLista(int n , int m){
+    Nodo<T> p,q,ant;
+    int i=0,may=0;
+    if(n>m){
+        may=n;
+    }else{
+        may=m;
+    }
+    this -> punta = new Nodo(n,m,0);
+    ant=punta;
+    for(int i=1;i<may;i++){
+        q = new Nodo(i,i,0);
+        q.asignarLF(q);
+        q.asignarLC(q);
+        ant.asignarLiga(q);
+        ant=q;
+    }
+    ant.asignarLiga(punta);
+}
+/*template<typename T>
+void DispersaLista<T>::insertarDato( int f, int c, T d){
+    Nodo<T> p,q;
+    int s;
+    p = buscar(n,m);
+    if(p==nullptr){
+        q= new Nodo(f,c,d);
+        ligarFila(q);
+        ligarColumna(q);
+    }else{
+        s=p.obtenerDato() + d;
+        if(s=0){
+            desligarFila(p);
+            desligarColumna(p);
+            libere(p);
+        }else{
+            p.asignarDato(s);
+        }
+    }
+}*/
+template<typename T>
+Nodo<T> DispersaLista<T>::buscar(int f, int c){
+    Nodo<T> p,q,res;
+    res=NULL;
+    p=punta.obtenerLiga();
+    while(p.obtenerFila() < f){
+        p=p.obtenerLiga();
+    }
+    q=p.obtenerLF();
+    while(q==p && q.obtenerColumna()<c){
+        q=q.obtenerLF();
+    }
+    if(q!=p && q.obtenerFila()=f && q.obtenerColumna()=c){
+        res=q;
+    }
+    return res;
+}
 
 #endif
